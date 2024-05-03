b23b689453d3ad23c0b53f792e0a43ffc83452ed
Xuanan999 <157749700+Xuanan999@users.noreply.github.com>
2024-05-03 21:04:44 +0700

    Update Tổng hợp script.lua

--- Tổng hợp script.lua
+++ Tổng hợp script.lua
@@ -68,41 +68,42 @@ Tab2:AddButton({
   	end    
 })
 -------webhook
-local HttpService = game:GetService("HttpService")
+local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Trigon" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "Fluxus" or
+"Fluxus"
+                    local HttpService = game:GetService("HttpService")
+                     local Data =
+                        {
+                            ["embeds"]= {
+                                {            
+                                    ["title"]= "Profile";
+                                    ["url"]= "https://www.roblox.com/users/"..game.Players.LocalPlayer.UserId;
+                                    ["description"]= "```"..game.Players.LocalPlayer.DisplayName.." ("..game.Players.LocalPlayer.Name..") ```";
+                                    ["color"]= tonumber(322852);
+                                    ["thumbnail"] = {["url"] = "https://cdn.discordapp.com/attachments/1214650224161726544/1235943849537568828/IMG_1714652535034_1714652539506.jpg?ex=663635e8&is=6634e468&hm=a5530ef6d0795650fb1590e218c3e56e0985481f3261dd5a406ed61a4d33bce5&"};
+                                    ["fields"]= {
+                                        {
+                                            ["name"]= "Execute",
+                                            ["value"]= "```"..ExecutorUsing.."```",
+                                            ["inline"]= true
+                                        },
+                                        {
+                                            ["name"]= "age",
+                                            ["value"]= "```"..game.Players.LocalPlayer.AccountAge.." Days```",
+                                            ["inline"]= true
+                                        },
+                                        {
+                                            ["name"]= "Ty For Use",
+                                            ["value"]= "```Script Tong Hop```",
+                                            ["inline"]= true
+                                        },
 
--- Xây dựng dữ liệu để gửi
-local Data = {
-    ["embeds"] = {
-        {            
-            ["title"] = "Profile",
-            ["url"] = "https://www.roblox.com/users/"..game.Players.LocalPlayer.UserId,
-            ["description"] = "```"..game.Players.LocalPlayer.DisplayName.." ("..game.Players.LocalPlayer.Name..") ```",
-            ["color"] = tonumber(322852),
-            ["thumbnail"] = {["url"] = "https://cdn.discordapp.com/attachments/1214650224161726544/1235943849537568828/IMG_1714652535034_1714652539506.jpg?ex=663635e8&is=6634e468&hm=a5530ef6d0795650fb1590e218c3e56e0985481f3261dd5a406ed61a4d33bce5&"},
-            ["fields"] = {
-                {
-                    ["name"] = "Execute",
-                    ["value"] = "```"..ExecutorUsing.."```",
-                    ["inline"] = true
-                },
-                {
-                    ["name"] = "Age",
-                    ["value"] = "```"..game.Players.LocalPlayer.AccountAge.." Days```",
-                    ["inline"] = true
-                },
-                {
-                    ["name"] = "Ty For Use",
-                    ["value"] = "```Script Tong Hop```",
-                    ["inline"] = true
-                },
-            }              
-        }
-    }
-}
-
--- Chuẩn bị và gửi yêu cầu HTTP
-local Headers = {["Content-Type"]="application/json"}
-local Encoded = HttpService:JSONEncode(Data)
-local Request = http_request or request or HttpPost or syn.request
-local Final = {Url = "https://discord.com/api/webhooks/1212727073123340329/jSrexDnBdFCFIqMJvcKLrEs6zPpnwyKCA5doqu8-mWyLxgyorR3ZQJglfaLvrSDR6H0W", Body = Encoded, Method = "POST", Headers = Headers}
-Request(Final)
+                                    }              
+                                }
+                            }
+                    }
+                    local Headers = {["Content-Type"]="application/json"}
+                    local Encoded = HttpService:JSONEncode(Data)
+                    
+                    Request = http_request or request or HttpPost or syn.request
+                    local Final = {Url = "https://discord.com/api/webhooks/1212727073123340329/jSrexDnBdFCFIqMJvcKLrEs6zPpnwyKCA5doqu8-mWyLxgyorR3ZQJglfaLvrSDR6H0W" , Body = Encoded, Method = "POST", Headers = Headers}
+                    Request(Final)
