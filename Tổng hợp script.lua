93a499d25b09619c3a3e61295308d40d0c5b39b0
Xuanan999 <157749700+Xuanan999@users.noreply.github.com>
2024-05-03 19:48:33 +0700

    Update Tổng hợp script.lua

--- Tổng hợp script.lua
+++ Tổng hợp script.lua
@@ -2,61 +2,61 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl
 
 local Window = OrionLib:MakeWindow({Name = "AnDepZai - BloxFruit", HidePremium = false, SaveConfig = true, ConfigFolder = "andepzai"})
 
-local Tab2 = Window:MakeTab({
+local Tab = Window:MakeTab({
 	Name = "Infor",
 	Icon = "rbxassetid://16157875068",
 	PremiumOnly = false
 })
 
-local Tab = Window:MakeTab({
+local Tab2 = Window:MakeTab({
 	Name = "Scripts No Key",
 	Icon = "rbxassetid://16157875068",
 	PremiumOnly = false
 })
+-----------------------------------------------------------------------------------------------------------------------------------------------------------Infor
+Tab:AddParagraph("Thông Báo","An Siêu Đẹp Trai")
+Tab:AddLabel("Yêu Lưu Gia Bảo Huhu")
+Tab:AddLabel("Script Tao Như Lồn")
+Tab:AddLabel("Script Này Auto Ban")
 
 Tab:AddButton({
+    Name = "Join Discord",
+	Callback = function()
+        setclipboard("https://discord.com/invite/srJVQj4H")
+  	end    
+})
+--------------------------------------------------------------------------------------------------------------------------------Scripts No Key
+Tab2:AddButton({
 	Name = "W-azure",
 	Callback = function()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
   	end    
 })
 
-Tab:AddButton({
+Tab2:AddButton({
     Name = "Redz Hub",
 	Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
   	end    
 })
 
-Tab:AddButton({
+Tab2:AddButton({
     Name = "Night Hub",
 	Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NIGHTHUBONTOP/Main/main/LoaderScript.lua"))()
   	end    
 })
 
-Tab:AddButton({
+Tab2:AddButton({
     Name = "Redz Hub",
 	Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
   	end    
 })
 
-Tab:AddButton({
+Tab2:AddButton({
     Name = "Min XT",
 	Callback = function()
         loadstring(game:HttpGet"https://raw.githubusercontent.com/Basicallyy/Basicallyy/main/MinGamingPremiumVietSub.lua")()
-  	end    
-})
-
-CoolParagraph:Set("Thông Báo", "Vạn Vật Thua Gia Bảo")
-Tab2:AddLabel("Yêu Lưu Gia Bảo Huhu")
-Tab2:AddLabel("Script Tao Như Lồn")
-Tab2:AddLabel("Script Này Auto Ban")
-
-Tab2:AddButton({
-    Name = "Join Discord",
-	Callback = function()
-        setclipboard("https://discord.com/invite/srJVQj4H")
   	end    
 })
