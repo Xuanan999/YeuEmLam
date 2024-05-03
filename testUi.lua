314c3dbb28817680238bfb5697874a66ef41a865
Xuanan999 <157749700+Xuanan999@users.noreply.github.com>
2024-05-03 20:49:12 +0700

    Add files via upload

---
+++ testUi.lua
@@ -0,0 +1,24 @@
+-- Load the DrRay library from the GitHub repository Library
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+
+-- Create a new window and set its title and theme
+local window = DrRayLibrary:Load("AnDepZai", "Default")
+
+-- Create the first tab with an image ID
+local tab1 = DrRayLibrary.newTab("Tab 1", "ImageIdHere")
+
+-- Add elements to the first tab
+tab1.newLabel("An Mạnh Hơn Goku")
+tab1.newButton("Redz Hub", "Khá Ngon", function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
+end)
+
+-- Create the second tab with a different image ID
+local tab2 = DrRayLibrary.newTab("Tab 2", "ImageIdLogoHere")
+
+-- Add elements to the second tab
+tab2.newLabel("Hello, this is Tab 2.")
+tab2.newButton("Button", "Prints Hello!", function()
+    print('Hello!')
+end)
