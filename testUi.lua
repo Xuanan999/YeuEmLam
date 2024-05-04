7012fbaf7f58f13ce61c7a2d7869560926afd4ee
Xuanan999 <157749700+Xuanan999@users.noreply.github.com>
2024-05-05 02:02:41 +0700

    Update testUi.lua

--- testUi.lua
+++ testUi.lua
@@ -1,24 +1,63 @@
--- Load the DrRay library from the GitHub repository Library
+--Ui Hơi Lỏ
 local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
 
--- Create a new window and set its title and theme
 local window = DrRayLibrary:Load("AnDepZai", "Default")
 
--- Create the first tab with an image ID
-local tab1 = DrRayLibrary.newTab("Tab 1", "ImageIdHere")
+--Tab 1
+local tab1 = DrRayLibrary.newTab("Main", "ImageIdHere")
 
--- Add elements to the first tab
+
 tab1.newLabel("An Mạnh Hơn Goku")
-tab1.newButton("Redz Hub", "Khá Ngon", function()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
-end)
+local x2Code = {
+ "EXP_5B",
+ "CONTROL",
+ "UPDATE11",
+ "XMASEXP",
+ "1BILLION",
+ "ShutDownFix2",
+ "UPD14",
+ "STRAWHATMAINE",
+ "TantaiGaming",
+ "Colosseum",
+ "Axiore",
+ "Sub2Daigrock",
+ "Sky Island 3",
+ "Sub2OfficialNoobie",
+ "SUB2NOOBMASTER123",
+ "THEGREATACE",
+ "Fountain City",
+ "BIGNEWS",
+ "FUDD10",
+ "SUB2GAMERROBOT_EXP1",
+ "UPD15",
+ "2BILLION",
+ "UPD16",
+ "3BVISITS",
+ "fudd10_v2",
+ "Starcodeheo",
+ "Magicbus",
+ "JCWK",
+ "Bluxxy",
+ "Sub2Fer999",
+ "Enyu_is_Pro"
+}
+    
+    tab1.newButton("Redeem Code", "Nhập All Code Blox Fruit", function()
+        function RedeemCode(value)
+            game:GetService("ReplicatedStorage").Remotes.Redeem:InvokeServer(value)
+        end
+        for i,v in pairs(x2Code) do
+            RedeemCode(v)
+        end
+    end)
+--Tab 2
+local tab2 = DrRayLibrary.newTab("Chưa Nghĩ", "ImageIdLogoHere")
 
--- Create the second tab with a different image ID
-local tab2 = DrRayLibrary.newTab("Tab 2", "ImageIdLogoHere")
 
--- Add elements to the second tab
-tab2.newLabel("Hello, this is Tab 2.")
-tab2.newButton("Button", "Prints Hello!", function()
-    print('Hello!')
-end)
+tab2.newLabel("Chức Năng Beta")
+tab2.newButton("HetCuu", "Thử Là Biết", function()
+    Setclipboard("sống ở đời tò Mò Ít Thôi")
+    DiscordLib:Notification("Thông Báo" , "Đã Sao Chép Link Facebook" , 10 ,17196546025 )
+ end)
+
