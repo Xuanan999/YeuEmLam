5b60ce0f3a0b4dd9da1a68a8a7ab61cbb33354c6
Xuanan999 <157749700+Xuanan999@users.noreply.github.com>
2024-05-07 20:51:22 +0700

    Update AnDepZai.lua

--- AnDepZai.lua
+++ AnDepZai.lua
@@ -1,54 +1,34 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
-OrionLib:MakeNotification({
-	Name = "Bot AnDepZai",
-	Content = "Mẹ Mày Béo",
-	Image = "rbxassetid://4483345998",
-	Time = 5
+local Window = Fluent:CreateWindow({
+    Title = "AnhYeuEm" .. Fluent.Version,
+    SubTitle = "True V2",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(370, 350),
+    Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
-local Window = OrionLib:MakeWindow({Name = "Gay Sex Fat", HidePremium = false, SaveConfig = true, IntroText = "ConCac", ConfigFolder = "OrionTest"})
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main",}),
+    Settings = Window:AddTab({ Title = "Settings",})
+}
 
-local Tab = Window:MakeTab({
-	Name = "MeMayBeo",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
---Tab 1--
-local Section = Tab:AddSection({
-	Name = "Thuốc Nổ"
+local Options = Fluent.Options
 
-Tab:AddButton({
-	Name = "Button!",
-	Callback = function()
-      		spawn(function() 
-    repeat
-        task.wait()
-    until game:IsLoaded()
-    repeat
-        task.wait()
-    until game.Players
-    repeat
-        task.wait()
-    until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
-    wait(1.5)
-    require(game.ReplicatedStorage.Notification).new("<Color=Red>MÀY  😂😂😂ĂN CON CẶC GÌ MÀ NGU VẬY CÓ THẾ CŨNG BỊ LỪA😂😂😂<Color=/>"):Display()
-    wait(.5)
-    require(game.ReplicatedStorage.Notification).new("<Color=Red>MÀY  😂😂😂ĂN CON CẶC GÌ MÀ NGU VẬY CÓ THẾ CŨNG BỊ LỪA😂😂😂<Color=/>"):Display()
-    wait(.14)
-    require(game.ReplicatedStorage.Notification).new("<Color=Yellow>0362374399<Color=/>"):Display()
-    wait(.24)
-    require(game.ReplicatedStorage.Notification).new("<Color=Yellow>MÀY  😂😂😂ĂN CON CẶC GÌ MÀ NGU VẬY CÓ THẾ CŨNG BỊ LỪA😂😂😂<Color=/>"):Display()
-    wait(.29)
-    require(game.ReplicatedStorage.Notification).new("<Color=Yellow>MÀY😂😂😂ĂN CON CẶC GÌ MÀ NGU VẬY CÓ THẾ CŨNG BỊ LỪA😂😂😂 <Color=/>"):Display()
-    wait(.36)
-end)spawn(function()
-    while wait() do
-        for i,v in pairs(game.Players.LocalPlayer:GetChildren()) do
-            if v.Name == "DataLoaded" or v.Name == "DataPreloaded" then
-                v:Destroy()
-            end
-        end
-    end
-end)
+do
+-------------------------------------------------------------------------------------------------------------------------------------------------Tab Main 
+local Slider = Tabs.Main:AddSlider("Slider", {
+        Title = "Walk Speed",
+        Description = "Tăng Tốc",
+        Default = 20,
+        Min = 16,
+        Max = 500,
+        Rounding = 1,
+        Callback = function(Value)
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+	end    
 })
